f23e15edcab69362541a16c659399b7c834f9d18
Harmans <harmans6282@gmail.com>
2025-12-14 17:36:06 +0530

    simple web server

--- go-crud/go.mod
+++ go-crud/go.mod
@@ -1,6 +1,6 @@
 module github.com/Harman6282/go-crud
 
-go 1.25.3
+go 1.25
 
 require (
 	github.com/gorilla/mux v1.8.1 // indirect
--- social-app/backend/cmd/api/main.go
+++ social-app/backend/cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/Harman6282/Go-Projects/social/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("ADDR", ":8080"),
 	}
 
 	app := &application{
@@ -12,6 +16,6 @@ func main() {
 	}
 
 	mux := app.mount()
- 
+
 	log.Fatal(app.run(mux))
 }
